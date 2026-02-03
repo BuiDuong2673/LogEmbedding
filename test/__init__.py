e9983f61611660a4eacafe570aeedf50f9bb1cf9
BuiDuong2673 <bachduong26723@gmail.com>
2026-02-03 01:35:54 +0100

    add test result analysis

--- test/__init__.py
+++ test/__init__.py
@@ -1,11 +1,11 @@
 from test import (
     find_similar_log_test,
     generate_similar_logs,
-    experiment_with_word2vec
+    test_result_analysis
 )
 
 __all__ = [
     "find_similar_log_test",
     "generate_similar_logs",
-    "experiment_with_word2vec"
+    "test_result_analysis"
 ]
