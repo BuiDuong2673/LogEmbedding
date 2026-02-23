9255ca286f2a5e803ea73fda9cf4c504828c93c8
BuiDuong2673 <bachduong26723@gmail.com>
2026-02-23 11:40:03 +0100

    add trained model with 2 negative samples and its test result

--- client_program.py
+++ client_program.py
@@ -203,14 +203,14 @@ class ClientProgram:
 
 if __name__ == "__main__":
     # Define the model name to save the word_dict to the model folder
-    model_name = "10_3_epochs_300_dimensions_1_context_3_negative_0001_learning_rate"
+    model_name = "10_3_epochs_300_dimensions_1_context_6_negative_0001_learning_rate"
     # Read the client name from system argument variables.
     if len(sys.argv) < 2:
         raise ValueError("Missing client name. Usage: python client_program.py <client_name>")
     client_name = sys.argv[1]
     # Call ClientProgram class to handle the traning process
     client_program = ClientProgram(
-        client_name=client_name, model_name=model_name, num_context_words=1, num_negative_samples=3)
+        client_name=client_name, model_name=model_name, num_context_words=1, num_negative_samples=6)
     client_program.training(
         num_epochs=3, learning_rate=0.001
     )
