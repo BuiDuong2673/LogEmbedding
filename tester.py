893a7a4a20184a7b2b53a0c704dbfd46371a26cc
BuiDuong2673 <bachduong26723@gmail.com>
2026-02-24 18:16:06 +0100

    add trained model with context window size 4 and 3 negative samples and its test result

--- tester.py
+++ tester.py
@@ -1,93 +1,39 @@
+import os
 import json
-import numpy as np
 
-class Tester:
-    def __init__(self):
-        """Initialize the class."""
-        self.W1 = np.load("models/W1.npy")
-        self.W2 = np.load("models/W2.npy")
-        with open("dataset/internal_central_vocab.json", "r", encoding="utf-8") as file:
-            self.full_vocab = json.load(file)
-        with open("dataset/global_vocab_index.json", "r", encoding="utf-8") as file:
-            self.global_vocab = json.load(file)
-    
-    def test_analogy(
-        self, center_word: str, pos_sample_word: str, neg_sample_word: str, top_k=3):
-        """Test analogy: a is to b as c is to ?"""
-        # Get indices representation of the words
-        for word, global_idx in self.global_vocab.items():
-            if word == center_word:
-                center_idx = self.full_vocab.get(f"{global_idx}")
-            elif word == pos_sample_word:
-                pos_sample_idx = self.full_vocab.get(f"{global_idx}")
-            elif word == neg_sample_word:
-                neg_sample_idx = self.full_vocab.get(f"{global_idx}")
+def aggregate_vocab(directory: str) -> dict:
+    """Aggregate all clients vocabs and form a new set of internal indices."""
+    client_list = ["client_1", "client_2", "client_3"]
+    client_vocabs = {}
+    # Read client_{i}_word_dict.json files in directory
+    for client in client_list:
+        file_path = os.path.join(directory, f"{client}_word_dict.json")
+        if os.path.exists(file_path):
+            with open(file_path, "r", encoding="utf-8") as json_file:
+                client_vocabs[client] = json.load(json_file)
+
+    indice_map = {}
+    reserved_index = 0
+    common_words = set()
+    common_count = 0
+    for client, client_word_indices in client_vocabs.items():
+        print(f"Client {client}, number of words: {len(client_word_indices)}")
+        for word_index in client_word_indices:
+            if word_index.startswith("unk_"):
+                # Make unknown words unique per client
+                key = f"{client}_{word_index}"
+                if key not in indice_map:
+                    indice_map[key] = reserved_index
+                    reserved_index += 1
             else:
-                continue
-        if center_idx == None or pos_sample_idx == None or neg_sample_idx == None:
-            print(f"Error: cannot perform test because some words not found. center_idx = {center_idx},"
-                  f"pos_sample_idx = {pos_sample_idx} and neg_sample_idx = {neg_sample_idx}")
-            return
-        
-        # Get embeddings
-        center_embedding = self.W1[center_idx, :]
-        pos_sample_embedding = self.W1[pos_sample_idx, :]  
-        neg_sample_embedding = self.W1[neg_sample_idx, :]
+                if word_index not in indice_map:
+                    indice_map[word_index] = reserved_index
+                    reserved_index += 1
+                else:
+                    common_count += 1
+                    common_words.add(word_index)
+    print(f"Number of common words across clients: {len(common_words)}")
+    print(f"common_count = {common_count}")
+    print(f"Aggregated vocab: number of words: {len(indice_map)}")
 
-        norm_center = np.linalg.norm(center_embedding)
-        norm_pos = np.linalg.norm(pos_sample_embedding)
-        norm_neg = np.linalg.norm(neg_sample_embedding)
-
-        if norm_center > 0 and norm_pos > 0 and norm_neg > 0:
-            cosine_sim_pos = np.dot(center_embedding, pos_sample_embedding) / (norm_center * norm_pos)
-            cosine_sim_neg = np.dot(center_embedding, neg_sample_embedding) / (norm_center * norm_neg)
-        else:
-            cosine_sim_pos, cosine_sim_neg = 0, 0
-        
-        print(f"Cosine_sim between {center_word} and {pos_sample_word} is: {cosine_sim_pos}")
-        print(f"Cosine_sim between {center_word} and {neg_sample_word} is: {cosine_sim_neg}")
-    
-    def find_similar_words(self, word: str, top_k: int=10) -> None:
-        """Find k similar words."""
-        for global_word, global_idx in self.global_vocab.items():
-            if global_word == word:
-                word_idx = self.full_vocab.get(f"{global_idx}")
-        similarities = []
-        word_embedding = self.W1[word_idx, :]
-        # Cosine similarity: cos(θ) = (a·b)/(|a||b|)
-        norm_word = np.linalg.norm(word_embedding)
-        for _, internal_idx in self.full_vocab.items():
-            if internal_idx == word_idx:
-                continue
-            other_word_embedding = self.W1[internal_idx, :]
-            norm_other_word = np.linalg.norm(other_word_embedding)
-            if norm_word > 0 and norm_other_word > 0:
-                cosine_sim = np.dot(word_embedding, other_word_embedding) / (norm_word * norm_other_word)
-            else:
-                cosine_sim = 0
-            similarities.append((cosine_sim, internal_idx))
-        # Sort by similarity (descending)
-        similarities.sort(reverse=True)
-        print(f"\nWords most similar to '{word}':")
-        for i, (sim, similar_word_idx) in enumerate(similarities[:top_k]):
-            similar_word = None
-            for global_idx_str, internal_idx in self.full_vocab.items():
-                if similar_word_idx == internal_idx:
-                    for global_word, global_word_idx in self.global_vocab.items():
-                        if global_idx_str == f"{global_word_idx}":
-                            similar_word = global_word
-                            break
-                    if similar_word != None:
-                        break
-            print(f"{i+1}. '{similar_word}' (similarity: {sim:.3f})")
-
-
-if __name__ == "__main__":
-    tester = Tester()
-    # Test anology
-    tester.test_analogy("error", "value", "success")
-    print()
-    tester.test_analogy("error", "runtime", "success")
-    # Find similar words
-    tester.find_similar_words("failed", 5)
-    
+aggregate_vocab("models_balanced/10_3_epochs_300_dimensions_4_context_3_negative_0001_learning_rate")
